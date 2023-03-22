0cae30c70779a12ae2d1f482c8f41386c8c76d56
Joseph Thatcher <thatcherjoe20@gmail.com>
2023-03-22 18:36:49 -0400

    working but not returning the right number for paswword

--- js/script.js
+++ js/script.js
@@ -1,49 +1,62 @@
-const generateBtn = document.getElementById('#generateBtn');
+const generateBtn = document.querySelector("#generate");
 
-function randomNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function getRandomItem(list) {
-  return list[randomNum(0, list.length - 1)];
+  return list[randomInt(0, list.length - 1)];
 }
 
-function getPasswordLength() {
+function generatePassword() {
   const userInput = window.prompt("Please choose a number between 8 and 128 for your password length.");
   const passwordLength = parseInt(userInput);
-}
 
-if (isNaN(passwordLength)) {
-  window.alert("Please enter a number between 1-128.");
-  return getPasswordLength();
-}
-
-  getPasswordLength() {
-      const userInput = window.prompt("Please choose a number between 8 and 128 for your password length.");
-      const passwordLength = parseInt(userInput);
-    }
-
-  {
-    return passwordLength; 
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Enter a number between 8 and 128");
+    return null;
   }
 
-  function getPasswordOptions() {
-    const lowerCase = ['abcdefghijklmnopqrstuvwxyz'];
-    const upperCase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
-    const special = ['@%\\!#$?:,-_.'];
-    const numeric = ['0123456789'];
+  const lowerCase = ['abcdefghijklmnopqrstuvwxyz'];
+  const upperCase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+  const special = ['@%+\\/!#$^?:,~-_.'];
+  const numeric = ['0123456789'];
 
-    const options = {
-      lowerCase: window.confirm("Would you like to include lowercase letters?"),
-      upperCase: window.confirm("Would you like to include uppercase letters?"),
-      special: window.confirm("Would you like to include special characters?"),
-      numeric: window.confirm("Would you like to include numbers?")
-    };
+  const userLowerCase = window.confirm("Would you like to include lowercase letters?");
+  const userUpperCase = window.confirm("Would you like to include uppercase letters?");
+  const userSpecial = window.confirm("Would you like to include special characters?");
+  const userNumeric = window.confirm("Would you like to include numbers?");
 
-    if (!options.lowerCase && !options.upperCase && !options.special && !options.numeric) {
-      window.alert("Please select at least one character type to continue.");
-      return getPasswordOptions();
-    }
+  const passwordOptions = [];
 
-    return options;
+  userNumeric ? passwordOptions.push(numeric) : null;
+  userSpecial ? passwordOptions.push(special) : null;
+  userLowerCase ? passwordOptions.push(lowerCase) : null;
+  userUpperCase ? passwordOptions.push(upperCase) : null;
+
+  if (passwordOptions.length === 0) {
+    alert("You must select at least one choice.");
+    return null;
   }
+
+  let generatedPassword = "";
+
+  passwordOptions.forEach(option => {
+    generatedPassword += getRandomItem(option);
+  });
+
+  for (let i = generatedPassword.length; i < passwordLength; i++) {
+    const randomList = getRandomItem(passwordOptions);
+    const randomChar = getRandomItem(randomList);
+    generatedPassword += randomChar;
+  }
+  return generatedPassword;
+}
+
+function writePassword() {
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
